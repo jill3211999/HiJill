2b8eccfc382b14b4b3d6adf40746784aa6a99384
Jill <jill.huang@finsage.com.tw>
2018-08-17 08:02:47 +0800

    取得長度之後才進行上傳

--- js/index.js
+++ js/index.js
@@ -9,23 +9,25 @@ function submit(){
 	
 	firebase.database().ref().once('value').then(function(snapshot) {
 		nowIndex = Object.keys(snapshot.val()).length;
+
+		if (nowIndex != undefined && document.getElementById("questionTextarea").value != "") {
+			firebase.database().ref().update({
+			    [nowIndex] : document.getElementById("questionTextarea").value
+		  	}, function(error) {
+			    if (error) {
+			      // The write failed...
+			      	alert("送出有問題，請重新送出");
+			    }else{
+			    	alert("成功～");
+			    	document.getElementById("questionTextarea").value = ""
+			    }
+			});
+		}else{
+			alert("沒有送到，再送出一次");
+		};
 	});
 	
-	if (nowIndex != undefined) {
-		firebase.database().ref().update({
-		    [nowIndex] : document.getElementById("questionTextarea").value
-	  	}, function(error) {
-		    if (error) {
-		      // The write failed...
-		      	alert("送出有問題，請重新送出");
-		    }else{
-		    	alert("成功～");
-		    	document.getElementById("questionTextarea").value = ""
-		    }
-		});
-	}else{
-		alert("沒有送到，再送出一次");
-	};
+
 }
 
 function getNowIndex(){
